6b9b4fd9ad736eb1ee08fb1907f9a1a40fcac0fa
Nyasn <yannis.stefanelli@epitech.eu>
2022-09-10 06:54:58 +0200

    starting embed video on body

--- thecamp/src/components/Body.js
+++ thecamp/src/components/Body.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
 
 class Body extends React.Component {
-
+    render() {
+          return (
+            <>
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe class="embed-responsive-item" src="https://www.youtube.com/watch?v=eSYXrrB1DYM"></iframe>
+                </div>
+            </>
+          );
+      }
 }
 
 export default Body;
