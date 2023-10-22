846a70bb9d75b83a45f19dfcd542ac8e33471918
Ena <akaidenki@gmail.com>
2023-10-22 03:23:56 +0200

    api update fix

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Places from './components/Fetcher';
+import 'bootstrap/dist/js/bootstrap.bundle';
 
 function App() {
   return (
--- src/components/Card.js
+++ src/components/Card.js
@@ -5,24 +5,29 @@ import 'bootstrap/dist/js/bootstrap.js';
 import Gdisplay from '../functions/Gdisplay';
 
 class Card extends React.Component {
-    render () {
+    render() {
+        const { data, nb } = this.props;
+
+         if (!data || data.length <= nb || !data[nb]) {
+            // Les données ne sont pas trouvées, ne rien afficher
+            return null;
+        }
+
+        // Les données ont été trouvées, affichez le contenu
         return (
-        <>
-            <div className='grow'>
-                <div className="card border-0 card-description">
-                    <img src={this.props.data[this.props.nb].media.url} alt={this.props.data[this.props.nb].media.alt} className="item card" />
+            <div className='card-container'>
+                <div className='grow'>
+                    <div className="card border-0 card-description">
+                        <img src={data[nb].media.url} alt={data[nb].media.alt} className="item card" />
+                    </div>
+                    <Gdisplay data={data} nb={nb} />
+                    <div className='places-description' dangerouslySetInnerHTML={{ __html: data[nb].description }} />
                 </div>
-                <Gdisplay
-                    data={this.props.data}
-                    nb={this.props.nb}
-                />
-                <div className='places-description' dangerouslySetInnerHTML={{__html: this.props.data[this.props.nb].description}} />
+                <div className='flex-name'>
+                    <p>{data[nb].name}</p>
+                    <p className="flex-description">{data[nb].baseline}</p>
+                </div>
             </div>
-            <div className='flex-name'>
-                <p>{this.props.data[this.props.nb].name}</p>
-                <p className="flex-description">{this.props.data[this.props.nb].baseline}</p>
-            </div>
-        </>
         );
     }
 }
